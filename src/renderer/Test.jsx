55351ece2269d3b048fd0d7c8f5dafb549917266
shuzhang <zspishere@qq.com>
2021-07-05 09:34:41 +0800

    add react-activation

---
+++ src/renderer/Test.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import KeepAlive, { AliveScope } from 'react-activation'
+
+import { autoFixContext } from 'react-activation'
+import * as jsxRuntime from 'react/jsx-runtime'
+import * as jsxDevRuntime from 'react/jsx-dev-runtime'
+autoFixContext(
+  [jsxRuntime, 'jsx', 'jsxs', 'jsxDEV'],
+  [jsxDevRuntime, 'jsx', 'jsxs', 'jsxDEV']
+)
+
+function Counter() {
+  const [count, setCount] = useState(1)
+
+  return (
+    <div>
+      <p>count: {count}</p>
+      <button onClick={() => {
+        setCount(count => count + 1)
+        console.log('click ......')
+        }}>Add+1</button>
+    </div>
+  )
+}
+
+function Test() {
+  const [show, setShow] = useState(true)
+
+  return (
+    <AliveScope>
+      <div>
+        <button onClick={() => setShow(show => !show)}>Toggle</button>
+        {show && (
+          <KeepAlive>
+            <Counter />
+          </KeepAlive>
+        )}
+      </div>
+    </AliveScope>
+  )
+}
+
+export default Test
